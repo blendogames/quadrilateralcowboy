b81165d0dd98c954c1635ee2ce0f08bc5d228a2d
Oliver McFadden <omcfadde@gmail.com>
2012-08-17 08:26:13 +0300

    dante: define BUILD_STRING for Linux ARM support.
    Signed-off-by: Oliver McFadden <omcfadde@gmail.com>
    (cherry picked from commit 099825c2aa856ece5c3876553bc489c8bdf87168)

--- sys/sys_public.h
+++ sys/sys_public.h
@@ -142,6 +142,10 @@ If you have questions concerning this license or the applicable additional terms
 #elif defined(__ppc__)
 	#define	BUILD_STRING				"linux-ppc"
 	#define CPUSTRING					"ppc"
+#elif defined(__arm__)
+	#define	BUILD_STRING				"linux-arm"
+	#define BUILD_OS_ID					2
+	#define CPUSTRING					"arm"
 #else
 #error Unrecognized CPU arch!
 #endif
