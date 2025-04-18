f988c01ed64249e0d4790d285b0d3ccc3b444be1
Ethan Lee <flibitijibibo@gmail.com>
2025-04-18 15:41:35 -0400

    Fix a really odd crash caused by too many shader reloads on startup

--- renderer/RenderSystem_init.cpp
+++ renderer/RenderSystem_init.cpp
@@ -958,7 +958,8 @@ void R_InitOpenGL( void ) {
 	R_ARB2_Init();
 
 	cmdSystem->AddCommand( "reloadARBprograms", R_ReloadARBPrograms_f, CMD_FL_RENDERER, "reloads ARB programs" );
-	R_ReloadARBPrograms_f( idCmdArgs() );
+	// flibit removed this, r_gammaInShader handles this now
+	// R_ReloadARBPrograms_f( idCmdArgs() );
 
 	// allocate the vertex array range or vertex objects
 	vertexCache.Init();
